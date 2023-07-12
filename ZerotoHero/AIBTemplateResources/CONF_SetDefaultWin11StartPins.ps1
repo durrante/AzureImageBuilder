22a0cc1398f40b312712ef41c77e3acfd47d9d5c
durrante <64601521+durrante@users.noreply.github.com>
2023-07-12 18:45:42 +0100

    Create CONF_SetDefaultWin11StartPins.ps1

---
+++ ZerotoHero/AIBTemplateResources/CONF_SetDefaultWin11StartPins.ps1
@@ -0,0 +1,19 @@
+# Reference: Alex Durrant's blog (https://letsconfigmgr.com)
+# Script to set a default Start menu PIN layout for Windows 11 devices
+
+# Define URLs, file paths, and locations
+$StartBinURL = 'https://your-storage-account-url/start2.bin'
+$DownloadLocation = 'C:\Temp\start2.bin'
+$DestinationFolder = Join-Path -Path $env:SystemDrive -ChildPath 'Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState'
+$DestinationFile = Join-Path -Path $DestinationFolder -ChildPath 'start2.bin'
+
+# Download the start2.bin file
+Invoke-WebRequest -Uri $StartBinURL -OutFile $DownloadLocation
+
+# Create the destination folder if it doesn't exist
+if (-not (Test-Path -Path $DestinationFolder)) {
+    New-Item -Path $DestinationFolder -ItemType Directory -Force | Out-Null
+}
+
+# Copy the downloaded file to the destination
+Copy-Item -Path $DownloadLocation -Destination $DestinationFile -Force
