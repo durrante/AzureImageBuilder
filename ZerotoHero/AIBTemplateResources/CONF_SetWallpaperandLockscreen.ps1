9e330fcd19444c41699f51f68ffc4ac06876f20b
durrante <64601521+durrante@users.noreply.github.com>
2023-07-17 11:33:28 +0100

    Update CONF_SetWallpaperandLockscreen.ps1

--- ZerotoHero/AIBTemplateResources/CONF_SetWallpaperandLockscreen.ps1
+++ ZerotoHero/AIBTemplateResources/CONF_SetWallpaperandLockscreen.ps1
@@ -1,6 +1,16 @@
 # Reference: Alex Durrant's blog (https://letsconfigmgr.com)
 # Script to download images from public storage URLs and set desktop and lockscreen wallpapers on Windows machines
 
+# If we are running as a 32-bit process on an x64 system, re-launch as a 64-bit process
+if ("$env:PROCESSOR_ARCHITEW6432" -ne "ARM64")
+{
+    if (Test-Path "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe")
+    {
+        & "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy bypass -NoProfile -File "$PSCommandPath"
+        Exit $lastexitcode
+    }
+}
+
 # Define paths, URLs, and file locations
 $RegistryPath = "HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PersonalizationCSP"
 $BackgroundImageURL = '<URL TO YOUR BACKGROUND IMAGE>'
